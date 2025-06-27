62692114ee25f468ecd771bdc961a7a91d16b831
vblacik <victoriann@gmail.com>
2025-06-26 20:27:18 -0400

    updated units

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -2,7 +2,7 @@ function showweatherDetails(event) {
     event.preventDefault();
     const city = document.getElementById('city').value;
     const apiKey = '86832abffe8b898a648b7546ac956901'; // Replace 'YOUR_API_KEY' with your actual API key
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units-metric`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     
     fetch(apiUrl)
     .then(response => response.json())
