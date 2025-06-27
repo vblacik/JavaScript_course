f670705157b361d7172c985c5665a74e9b5c0e30
vblacik <39569900+vblacik@users.noreply.github.com>
2025-06-26 20:45:02 -0400

    Update weather_report.js
    security fix

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,7 +1,7 @@
 function showweatherDetails(event) {
     event.preventDefault();
     const city = document.getElementById('city').value;
-    const apiKey = '86832abffe8b898a648b7546ac956901'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiKey = 'OPEN WEATHER API KEY'; // Replace 'YOUR_API_KEY' with your actual API key
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     
     fetch(apiUrl)
